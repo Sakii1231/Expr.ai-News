e25f28dfdcd997a689b989eee43429332b3c1418
Sakshat Khadka <sakshat.khadka@WebsutraNepal.com>
2022-11-25 11:50:37 +0545

    docker

--- app/main.py
+++ app/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import multipart
 import nltk
 from textblob import TextBlob
 from newspaper import Article
 import uvicorn
+
 
 nltk.download('punkt')
 
@@ -13,6 +15,7 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request:Request):    
@@ -38,7 +41,7 @@ async def form_input(request:Request, URL: str = Form(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run("main:app", reload=True)
 
 
 
